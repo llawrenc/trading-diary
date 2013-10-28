5c7120114cc09c9cb97a8820d8287f089bcac396
Lloyd Lawrence <lloyd@lawrenceau.net>
2013-10-29 04:47:54 +1000

    framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradingDiary::Application.config.secret_token = '8af912671c16b8bec0317dd88378b26482d40d63e898db6975183a3ea42fdec8a925a70adf1e65b8a443420ac54e81a758a95488892b761a0df56f15532993ca'
